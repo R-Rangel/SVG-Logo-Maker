f6611cef693864a197d559feca80c2902364eac6
R-Rangel <r.rangel.0222@gmail.com>
2023-03-28 02:53:15 -0500

    fixing the main index changing it from es5 to es6

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # SVG-Logo-Maker
+     The task that I was given was to create a command prompt that prompts the user to make a svg logo. Using jest and inquire I was able to achieve it .
+
+## User Story
+
+```
+AS a freelance web developer
+I WANT to generate a simple logo for my projects
+SO THAT I don't have to pay a graphic designer.
+
+```
+
+
+## Acceptance Criteria
+
+```
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for text
+THEN I can enter up to three characters
+WHEN I am prompted for the text color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I am prompted for a shape
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+WHEN I am prompted for the shape's color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I have entered input for all the prompts
+THEN an SVG file is created named `logo.svg`
+AND the output text "Generated logo.svg" is printed in the command line
+WHEN I open the `logo.svg` file in a browser
+THEN I am shown a 300x200 pixel image that matches the criteria I entered
+```
+## HOW TO USE
+first type in 
+```
+node index.js
+```
+then you'll be prompted with the following: 
+
+```
+Enter up to three characters for the logo
+Enter a color keyword or hexadecimal number for the text
+Choose a shape
+Enter a color keyword or hexadecimal number for the shape
+```
+then the svg should appear in your bash
+
+## Screenshots
--- index.js
+++ index.js
@@ -1,53 +1,73 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
-const { Triangle, Circle, Square } = require("./lib/shapes");
+import { writeFile } from "fs";
+import { createPromptModule } from "inquirer";
+
+import { Triangle, Circle, Square } from "./lib/shapes.js";
 
 const shapes = [Triangle, Circle, Square].map((Shape) => new Shape());
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "text",
-      message: "Enter up to three characters for the logo:",
-    },
-    {
-      type: "input",
-      name: "textColor",
-      message: "Enter a color keyword or hexadecimal number for the text:",
-    },
-    {
-      type: "list",
-      name: "shape",
-      message: "Choose a shape:",
-      choices: ["Triangle", "Circle", "Square"],
-    },
-    {
-      type: "input",
-      name: "shapeColor",
-      message: "Enter a color keyword or hexadecimal number for the shape:",
-    },
-  ])
-  .then((answers) => {
-    const shape = shapes.find((s) => s.constructor.name === answers.shape);
-    shape.setColor(answers.shapeColor);
+class LogoMaker {
+  constructor() {
+    this.text = "";
+    this.textColor = "";
+    this.shape = null;
+    this.shapeColor = "";
+  }
+
+  async promptUser() {
+    const prompt = createPromptModule();
+    const answers = await prompt([
+      {
+        type: "input",
+        name: "text",
+        message: "Enter up to three characters for the logo:",
+      },
+      {
+        type: "input",
+        name: "textColor",
+        message:
+          "Enter a color keyword or hexadecimal number for the text:",
+      },
+      {
+        type: "list",
+        name: "shape",
+        message: "Choose a shape:",
+        choices: ["Triangle", "Circle", "Square"],
+      },
+      {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter a color keyword or hexadecimal number for the shape:",
+      },
+    ]);
+  
+    this.text = answers.text;
+    this.textColor = answers.textColor;
+    this.shape = shapes.find((s) => s.constructor.name === answers.shape);
+    this.shapeColor = answers.shapeColor;
+  
+    this.createLogo();
+  }
+  
+
+  createLogo() {
+    this.shape.setColor(this.shapeColor);
 
     const svg = `
       <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-        ${shape.render()}
-        <text x="150" y="110" fill="${answers.textColor}" text-anchor="middle" font-size="80">${answers.text}</text>
+        ${this.shape.render()}
+        <text x="150" y="110" fill="${this.textColor}" text-anchor="middle" font-size="80">${this.text}</text>
       </svg>
     `;
 
-    fs.writeFile("logo.svg", svg, (err) => {
-        if (err) {
-            console.error(err);
-            } else {
-            console.log("Logo successfully created!");
-            }
-            });
-            })
-            .catch((error) => {
-            console.error(error);
-            });
-     
+    writeFile("logo.svg", svg, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("Generated logo.svg");
+      }
+    });
+  }
+}
+
+const logoMaker = new LogoMaker();
+logoMaker.promptUser();
